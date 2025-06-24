2d136e270d300b5bc06b4a67a52f955e27ae8e0d
VoteVeto2 <xinyu.yang@student.kuleuven.be>
2025-06-24 21:18:59 +0000

    Deploying to gh-pages from @ VoteVeto2/VoteVeto2.github.io@36ab98b496d9811e754f307b8ec02dc6c8311e4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-knowledge-distill",
+        },{id: "post-pitch-for-thesis-topic",
+      
+        title: "Pitch For Thesis Topic",
+      
+      description: "this is for the Science Communication project",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Pitch/";
+        
+      },
+    },{id: "post-knowledge-distill",
       
         title: "Knowledge Distill",
       
